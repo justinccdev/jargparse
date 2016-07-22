30d7c6803a12cc9843727d62242196d4c84b722d
Justin Clark-Casey <justincc@justincc.org>
2016-07-22 17:34:06 +0100

    bump to 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'jargparse',
   packages = ['jargparse'], # this must be the same as the name above
-  version = '0.0.2',
+  version = '0.0.3',
   description = 'A tiny super-dumb python module just because I like to see the usage info on stdout on an error',
   author = 'Justin Clark-Casey',
   author_email = 'justincc@justincc.org',
