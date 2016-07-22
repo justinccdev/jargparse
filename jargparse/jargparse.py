e14e08f38af9bb35dcf670c0ea0b7b0c3ad0141d
Justin Clark-Casey <justincc@justincc.org>
2016-07-22 17:50:37 +0100

    Move ArgParser to jargparse.ArgParser for simplicity

--- jargparse/jargparse.py
+++
@@ -1,8 +0,0 @@
-import argparse
-import sys
-
-class ArgParser(argparse.ArgumentParser):
-    def error(self, message):
-        sys.stderr.write('error: %s\n' % message)
-        self.print_help()
-        sys.exit(2)
