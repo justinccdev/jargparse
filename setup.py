35cf4059bfc9bd2efb89f25e1a73324489e69510
Justin Clark-Casey <justincc@justincc.org>
2016-07-22 17:13:03 +0100

    Ooops, change mypackage to jargparse

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
   name = 'jargparse',
-  packages = ['mypackage'], # this must be the same as the name above
+  packages = ['jargparse'], # this must be the same as the name above
   version = '0.0.1',
   description = 'A tiny super-dumb python module just because I like to see the usage info on stdout on an error',
   author = 'Justin Clark-Casey',
